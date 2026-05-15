906c97d8591cbc693ce6e0d19654e32b3cd2dc01
pedrodrv <pedro.delreal@estudiante.uc.cl>
2026-05-15 17:14:13 -0400

    texto cursos

---
+++ src/pages/textosCursos.js
@@ -0,0 +1,29 @@
+// textosCursos.js
+
+export const textoFisicaLong = `
+    <b>Física II: Ondas y Calor</b><br /><br />
+    Este curso profundiza en los fenómenos macroscópicos de la materia, centrándose en cómo la energía se transfiere y se manifiesta a través de ondas y calor. Es la base para entender desde el funcionamiento de un motor hasta el comportamiento de la luz.<br /><br />
+    
+    <b>Ejes Principales del Curso:</b><br />
+    <ul>
+        <li><b>Termodinámica:</b> Estudio de las leyes que rigen el calor, el trabajo y la energía interna. Analizamos procesos adiabáticos, isobáricos y el concepto de entropía.</li>
+        <li><b>Movimiento Oscilatorio:</b> Descripción matemática del Movimiento Armónico Simple (MAS) y sistemas masa-resorte.</li>
+        <li><b>Ondas Mecánicas:</b> Propagación de energía a través de medios elásticos, cuerdas y sonido.</li>
+    </ul>
+    <br />
+    <b>Dato clave:</b> Es fundamental dominar las ecuaciones diferenciales básicas, ya que la física de ondas se describe casi totalmente a través de ellas. ¡No dejes que los diagramas de fase te dejen <b>ultra qliao</b> al final del semestre!
+`;
+
+export const textoCalculoLong = `
+    <b>Cálculo III: Cálculo Multivariable</b><br /><br />
+    En este curso dejamos el plano bidimensional para entrar en el espacio. Estudiamos funciones de varias variables y cómo cambian estas respecto a diferentes ejes simultáneamente. Es donde las matemáticas se vuelven realmente visuales y aplicadas a la ingeniería.<br /><br />
+    
+    <b>Contenidos Clave:</b><br />
+    <ul>
+        <li><b>Derivadas Parciales y Vectoriales:</b> Uso del Gradiente, Divergencia y Rotor para entender campos vectoriales.</li>
+        <li><b>Integrales Múltiples:</b> Cálculo de volúmenes y centros de masa mediante integrales dobles y triples en coordenadas polares, cilíndricas y esféricas.</li>
+        <li><b>Teoremas Fundamentales:</b> El núcleo del curso. Aquí aplicamos el <b>Teorema de Green</b>, el <b>Teorema de Stokes</b> y el de la <b>Divergencia (Gauss)</b>.</li>
+    </ul>
+    <br />
+    <b>Recomendación:</b> Si no dominas bien el cambio de coordenadas, el curso se vuelve <b>super qliao</b>. Practica mucho la visualización de sólidos en 3D para que no te pierdas en los límites de integración.
+`;
