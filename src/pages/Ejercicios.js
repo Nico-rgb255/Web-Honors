4937159eb25faf26ac180aa27beb24984d4b4280
Gianluca <gianluca.penna@estudiante.uc.cl>
2026-05-15 00:44:17 -0400

    Ejercicios 6

--- src/pages/Ejercicios.js
+++ src/pages/Ejercicios.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "../components/Header";
 import GetLocalStorage from "../LocalStorage/Get";
 import { useState } from "react";
+import '../styles/Ejercicios.css'
 
 function Ejercicios() {
     const [actual_state, setActivado] = useState(GetLocalStorage());
 
+    // LISTA EJERCICIOS
     const lista_ejercicios = [
         { id: 1, ramo: 'Cálculo III', enunciado: 'Enunciado', solucion: 'Solución', temario: 'Temario' },
         { id: 2, ramo: 'Cálculo III', enunciado: 'Enunciado', solucion: 'Solución', temario: 'Temario' },
@@ -13,14 +15,16 @@ function Ejercicios() {
         { id: 4, ramo: 'Ecuaciones Diferenciales', enunciado: 'Enunciado', solucion: 'Solución', temario: 'Temario' },
     ];
 
+    // VARIABLE DE FILTROS
     const [filtro, setFiltro] = useState("No-filtro");
 
+    // FILTRAJE DE EJERCICIOS
     const ejerciciosfiltrados = filtro === "No-filtro"
         ? lista_ejercicios
         : lista_ejercicios.filter(ej => ej.ramo === filtro);
 
     return (
-        <>
+        <div className={actual_state ? "oej" : "cej"}>
             <Header update={setActivado} state={actual_state} />
             <div style={titulo}>
                 <h1>Ejercicios</h1>
@@ -33,7 +37,7 @@ function Ejercicios() {
                 <button onClick={() => setFiltro("Ecuaciones Diferenciales")}>Ecuaciones Diferenciales</button>
             </div>
 
-            {ejerciciosfiltrados.map((ej) => ( //{lista_ejercicios.filter(ej => ej.ramo == 'Cálculo III').map((ej) => (
+            {ejerciciosfiltrados.map((ej) => (
                 <div key={ej.id} style={contenedor_ejercicios}>
                     <h2>{ej.ramo}</h2>
                     <p style={temario}>{ej.temario}</p>
@@ -41,7 +45,7 @@ function Ejercicios() {
                     <p>{ej.solucion}</p>
                 </div>
             ))}
-        </>
+        </div>
     );
 }
 
@@ -60,7 +64,7 @@ const contenedor_ejercicios = {
     padding: '20px',
     borderRadius: '15px',
     gap: '10px',
-    border: '4px solid #333',
+    border: '4px solid',
     margin: '10px'
 }
 
@@ -72,7 +76,7 @@ const temario = {
     padding: '20px',
     borderRadius: '15px',
     gap: '10px',
-    border: '2px solid #333',
+    border: '2px solid',
     margin: '10px'
 }
 
